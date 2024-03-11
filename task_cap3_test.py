81b105fd45206678efb9aee269c61947d49ed112
Kardo Jankowitz <kardojv@gmail.com>
2024-03-11 12:35:04 +0200

    login feature working

--- task_cap3_test.py
+++ task_cap3_test.py
@@ -1,36 +1,30 @@
-# Define Functions
+# Login feature
 
-def reg_user():
-    print("Enter credentials of new user.")
-    while True:
-        new_user = input("Enter new username:\n")
-        if new_user not in users_info:
-            break
-        print("Username already exists. Please use another.")
-    
-    while True:
-        new_pass = input("Enter new password:\n")
-        repeat_pass = input("Confirm password: ")
-
-        if new_pass == repeat_pass:
-            with open("19 Cap2/user.txt", "a", encoding = "utf-8") as file:
-                file.write(f"\n{new_user}, {new_pass}")
-            break
-        print("Passwords do not match. Please try again.")
-        
-    return "Registration successful."
-     
-    
 # Read user.txt and add all info to dictionary as username:password key-values
 users_info = {}
 
-with open("19 Cap2/user.txt", "r", encoding = "utf-8") as file:
+with open("user.txt", "r", encoding = "utf-8") as file:
 
     for line in file:
         single_line = line.strip()
         single_line = single_line.split(", ")
         users_info[single_line[0]] = single_line[1]
 
-print(users_info)      
+print(users_info)
 
-print(reg_user())
+
+while True:
+    print("Welcome to the TaskManager program! Please enter login details:")
+    USER_NAME = input("Username: ")
+    pass_word = input("Password: ")
+
+# Check for valid username, then valid password
+
+    if USER_NAME in users_info:
+        if pass_word == users_info[USER_NAME]:
+            print("Login successful.")
+            break
+        else:
+            print("Invalid password. Try again")
+    else: 
+        print("Invalid username. Try again.")
--- task_manager.py
+++ task_manager.py
@@ -3,28 +3,62 @@ from datetime import date
 
 # Define Functions
 
-#def reg_user():
-
+def reg_user():
+    '''
+    This function asks user to input new credentials to be registered as
+    new user. It will loop until a unique username and confirmed matching 
+    password is entered
+    '''
+    print("Enter credentials of new user.")
+    while True:
+        new_user = input("Enter new username:\n")
+        if new_user not in users_info:
+            break
+        print("Username already exists. Please use another.")
     
+    while True:
+        new_pass = input("Enter new password:\n")
+        repeat_pass = input("Confirm password: ")
+
+        if new_pass == repeat_pass:
+            with open("19 Cap2/user.txt", "a", encoding = "utf-8") as file:
+                file.write(f"\n{new_user}, {new_pass}")
+            break
+        print("Passwords do not match. Please try again.")
+        
+    return "Registration successful."
+   
 # Login section
 
-# Read user.txt and add all info to dictionary as username:password key-values    
+# Read user.txt and add all info to dictionary as username:password key-values
+users_info = {}
 
-# while True:
-#     print("Welcome to the TaskManager program! Please enter login details:")
-#     USER_NAME = input("Username: ")
-#     pass_word = input("Password: ")
+with open("user.txt", "r", encoding = "utf-8") as file:
 
-#     if USER_NAME in usernames and pass_word in passwords:
-#         print("Login successful.")
-#         break
+    for line in file:
+        single_line = line.strip()
+        single_line = single_line.split(", ")
+        users_info[single_line[0]] = single_line[1]
 
-#     # Check for valid username, then valid password
-#     if USER_NAME not in usernames:
-#         print("Invalid username. Try again.")
-#     elif pass_word not in passwords:
-#         print("Invalid password. Try again")
+print(users_info)
 
+
+while True:
+    print("Welcome to the TaskManager program! Please enter login details:")
+    USER_NAME = input("Username: ")
+    pass_word = input("Password: ")
+
+# Check for valid username, then valid password
+
+    if USER_NAME in users_info:
+        if pass_word == users_info[USER_NAME]:
+            print("Login successful.")
+            break
+        else:
+            print("Invalid password. Try again")
+    else: 
+        print("Invalid username. Try again.")
+        
 # #====Menu Section====
 # # Repeat menu presentation with while loop
 # while True:
